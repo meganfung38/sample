0bbcc90463b2b6c7d8a75c57225514a2621133bf
Megan Fung <emailmeganfung@gmail.com>
2025-10-29 13:07:11 -0700

    initial commit

---
+++ src/loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+        while (true) {
+            break;
+        }
+    }
+}
+// size: 12
+// complexity: 2
